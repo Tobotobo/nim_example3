93824681cc4754d76d2dac286df4defe257c7cbf
Tobotobo <tobotobo.channel@gmail.com>
2023-08-04 12:57:38 +0900

    多段の継承

---
+++ src/example6.nim
@@ -0,0 +1,16 @@
+type
+  A = ref object of RootObj
+  B = ref object of A
+  C = ref object of B
+
+method sayHello(self: A) {.base.} = echo "A"
+method sayHello(self: B) = echo "B"
+method sayHello(self: C) = echo "C"
+
+var aa:A = A()
+var ab:A = B()
+var ac:A = C()
+
+aa.sayHello()
+ab.sayHello()
+ac.sayHello()
